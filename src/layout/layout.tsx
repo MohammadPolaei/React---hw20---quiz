9ddba01fb70ae697d3a50921eb02bcb815bcc7e7
Mohammad Polaei <mohammadpolaei@gmail.com>
2026-01-04 13:07:41 +0330

    feat:App layout created

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -4,10 +4,10 @@ import Header from "./header";
 type IProps = { children: ReactNode };
 function AppLayout({ children }: IProps) {
 	return (
-		<>
+		<div className="bg-[#9334ea] text-white">
 			<Header />
 			{children}
-		</>
+		</div>
 	);
 }
 
